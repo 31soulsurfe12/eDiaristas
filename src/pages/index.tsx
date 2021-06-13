2011dfdf90c973642519f0bb829ad6e333c66a42
Soulsurfe <soulsurfe18@gmail.com>
2021-06-13 11:05:42 -0300

    front-end4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
-import UserInformation from "ui/components/data-display/UserInformation/Userinformation";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@material-ui/core";
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from "ui/styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -12,13 +19,37 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionanis da sua localidade"
         }
       />
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999-999"}
+            label={"Digite seu Cep"}
+            fullWidth
+            variant={"outlined"}
+          />
 
-      <UserInformation
-        name={"Flavio de souza"}
-        picture={"https://github.com/soulsurfe.png"}
-        rating={3}
-        description={"Fortaleza"}
-      />
+          <Typography color={"error"}>Cep Inválido</Typography>
+
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              name={"Flavio de souza"}
+              picture={"https://github.com/soulsurfe.png"}
+              rating={3}
+              description={"Fortaleza"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 }
