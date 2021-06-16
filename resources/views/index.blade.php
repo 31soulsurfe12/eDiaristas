d05b829db010fa38d22258e0623287eb49335427
Soulsurfe <soulsurfe18@gmail.com>
2021-06-15 21:42:55 -0300

    backend2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,37 +1,30 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+@section('titulo', 'Página inicial')
 
-    <title>Página Inicial</title>
-  </head>
-  <body>
-    <h1>Página inicial</h1>
-
-    <div class="container">
-
+@section('conteudo')
+ <h1>Lista de Diaristas</h1>
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">id</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Telefone</th>
-      <th scope="col">Ações</th>
-    </tr>
-  </thead>
-  <tbody>
+        <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Telefone</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+      <tbody>
 
     @forelse ($diaristas as $diaristas)
         <tr>
             <th scope="row">{{ $diaristas->id }}</th>
             <td>{{ $diaristas->nome_completo }}</td>
             <td>{{ $diaristas->telefone }}</td>
-            <td></td>
+            <td>
+
+                <a href="{{ route('diaristas.edit', $diaristas) }}" class="btn btn-primary">Atualizar</a>
+                <a href="{{ route('diaristas.destroy', $diaristas) }}" class="btn btn-danger" onclick="return confirm('Tem certeza que deseja apagar?')">Apagar</a>
+            </td>
         </tr>
     @empty
         <tr>
@@ -41,20 +34,8 @@
             <td></td>
         </tr>
     @endforelse
-  </tbody>
-</table>
-    </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+      </tbody>
+    </table>
+    <a href="{{ route('diaristas.create')}}" class="btn btn-success">Nova Diarista</a>
+@endsection
 
